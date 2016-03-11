5677579703949ea8eab289a22770993fc562090f
Peter S <s@pramos.me>
2016-03-11 14:36:20 +0100

    Completed logic for step 9

--- app/components/Detail.js
+++ app/components/Detail.js
@@ -1,8 +1,15 @@
 var React = require('react');
+var parser = require('../utils/parser');
 function Detail (props) {
-  console.log(props.location.state.weather);
   return (
-    <p>Detail!</p>
+    <div>
+      <h1>{props.routeParams.city}</h1>
+      <p>{parser.getDate(props.location.state.weather.dt)}</p>
+      <p>{props.location.state.weather.clouds} Clouds.</p>
+      <p>{props.location.state.weather.weather[0].description}</p>
+      <p>Min temp: {parser.convertTemp(props.location.state.weather.temp.min)} degrees.</p>
+      <p>Max temp: {parser.convertTemp(props.location.state.weather.temp.max)} degrees.</p>
+    </div>
   )
 }
 module.exports = Detail;
